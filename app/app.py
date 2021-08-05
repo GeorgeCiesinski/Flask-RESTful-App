7fec2d045f8931a85c8b7189bce79a6e6fa11d3d
George Ciesinski <george.ciesin@gmail.com>
2021-08-05 16:03:28 -0400

    Updated Item to use sqlite3 instead of a list

--- app/app.py
+++ app/app.py
@@ -20,5 +20,6 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
-# Start app
-app.run(port=5000, debug=True)
+if __name__ == '__main__':
+	# Start app
+	app.run(port=5000, debug=True)
